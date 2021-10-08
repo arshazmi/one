d1535205dcfdaef1d7ea1a8ac7d5e1bc44c2f21b
Arsheena <aazmi600@gmail.com>
2021-10-08 13:05:43 +0530

    top rated topics completed view and logic

--- frontend/src/app/sectionbody/sectionbody.component.ts
+++ frontend/src/app/sectionbody/sectionbody.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RightService } from '../right.service';
+import { ActivatedRoute,Router } from '@angular/router';
 @Component({
   selector: 'app-sectionbody',
   templateUrl: './sectionbody.component.html',
   styleUrls: ['./sectionbody.component.css']
 })
 export class SectionbodyComponent implements OnInit {
-
-  constructor() { }
+   Topics=[{
+    id: '',
+    postName: '',
+    imageUrl: '',
+    pdfUrl: '',
+    audioUrl: '',
+    desc:'',
+    user:'',
+    topic:'', 
+    postengage:'' 
+   }]
+  constructor(private router:Router,private route: ActivatedRoute,
+    private _Service:RightService) { }
 
   ngOnInit(): void {
+    this._Service.getTopics().subscribe((data)=>{
+      console.log(data);
+      this.Topics=JSON.parse(JSON.stringify(data));
+      
+  })
   }
 
 }
