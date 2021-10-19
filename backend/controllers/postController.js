ca3d27288fa50ac96a75dc94995a394172734b6c
dawti <dawtiej@gmail.com>
2021-10-19 20:02:56 +0530

    file/files create post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -15,13 +15,14 @@ const sequelize=model.db.sequelize;
 // Create and Save a new Post
 exports.create = (req, res) => {
  console.log("create post",req.files,req.body)
+ console.log(req.files.bimage,req.files.fieldname==='bimage'?req.files.bimage[0].path:"ok")
   // Create a Post
   const post = {
     postName: req.body.postName,
     desc:req.body.desc,
-    imageUrl: req.files. fieldname==='bimage'?req.files.bimage[0].path:null,
-    audioUrl:req.files. fieldname==='baudio'?req.files.baudio[0].path:null,
-    pdfUrl:req.files. fieldname==='bpdf'?req.files.bpdf[0].path:null,
+    imageUrl: req.files.bimage!==undefined?req.files.bimage[0].path:null,
+    audioUrl:req.files.baudio!==undefined?req.files.baudio[0].path:null,
+    pdfUrl:req.files.bpdf!==undefined?req.files.bpdf[0].path:null,
     topicId:req.body.topicId,
     userId:Math.floor(Math.random() * 2)+1
   };
